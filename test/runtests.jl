6a3a64280f51b30e15020b4fc0787ca8393e1ac3
bbonga <b.p.bonga@gmail.com>
2018-09-21 17:04:16 -0400

    Added extra test cases.

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,21 +8,29 @@ using Example2018
 # 4. addition commutes
 #HW add stuff
 
-@testset "Polynomials form a ring" begin 
+@testset "Properties polynomial additions are all satisfied." begin
 
 
 	z = Polynomial([0])
+	zz = Polynomial([0,0])
 
 	for n in 1:100
 		deg = rand(0:100)
 		p = Polynomial(rand(-10000000:10000000,deg+1))
+		q = Polynomial(rand(-10000000:10000000,deg+1))
+		n = Polynomial(rand(-10000000:10000000,deg+1))
 
-		@test p + z == p
-		@test z + p == p
-		@test p + (-p) == z
+
+		@test p + z == p #Additive identity element
+		@test z + p == p #Additive identity element commutes
+		@test p + (-p) == z #Additive inverse
+		@test p + (q + n) == (p + q) + n #Addition is associative
+		@test q + zz == q #Additive identity element of different length
+		@test zz + q == q #Additive identity element commutes
+		@test n + (-n) == zz #Additive inverse (of different length indentity)
 	end
 end
 
 
 1 == 1
-@test 2 == 2 
+@test 2 == 2
