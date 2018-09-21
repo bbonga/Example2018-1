3273f1848a87eab7c56a0e635fa3d249bb45467e
bbonga <b.p.bonga@gmail.com>
2018-09-21 15:00:25 -0400

    Included extra tests and all the other stuff we did in the lecture.

--- src/Example2018.jl
+++ src/Example2018.jl
@@ -34,9 +34,17 @@ function evaluate(p::Polynomial, x)
     sum(c * x^(i-1) for (i,c) in enumerate(p.coeffs))
 end
 
+
+function Base. ==(p::Polynomial, q::Polynomial)
+	d = p - q
+	#all(c -> c ==0, d.coeffs)
+	all(d.coeffs .== 0)
+end
+
+
 function Base. +(p::Polynomial, q::Polynomial)
     l = max(length(p.coeffs), length(q.coeffs))
-    rcoeffs = zeros(l)
+    rcoeffs = zeros(eltype(p.coeffs),l)
     for (i,c) in enumerate(p.coeffs)
         rcoeffs[i] += c
     end
@@ -45,6 +53,16 @@ function Base. +(p::Polynomial, q::Polynomial)
     end
     Polynomial(rcoeffs)
 end
+
+
+function Base. -(p::Polynomial)
+	(-1) * p
+end
+
+function Base. -(p::Polynomial,q::Polynomial)
+	p+ (-q)
+end
+
 
 function Base. *(a, p::Polynomial)
     Polynomial(map(x->a*x, p.coeffs))
